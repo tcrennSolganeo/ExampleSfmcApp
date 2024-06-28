40f8decf0440ea34609d6613fea34ac7dd1f4c78
Thomas Crenn <t.crenn@solganeo.com>
2024-06-28 16:31:04 +0200

    Test Postgres DB

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const router = express.Router();
+const pg = require('pg');
 
 const subdomain = process.env.subdomain;
 
@@ -28,6 +29,15 @@ router.get('/', async (req, res) => {
                 console.error('Failed to read the HTML file', err);
                 res.status(500).send('An error occurred while reading the HTML file.');
             } else {
+
+                pg.connect(process.env.DATABASE_URL, function(err, client) {
+                    var query = client.query('SELECT table_schema,table_name FROM information_schema.tables;');
+                    
+                    query.on('row', function(row) {
+                        console.log(JSON.stringify(row));
+                    });
+                });
+
                 const updatedHtml = html.replace('<pre id="response"></pre>', `<pre id="response">${JSON.stringify(response.data, null, 2)}</pre>`);
                 res.send(updatedHtml);
             }
