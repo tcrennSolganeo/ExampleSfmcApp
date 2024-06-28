1fff908fa6826a6d87f83d8a2941081d92aeacc8
Thomas Crenn <t.crenn@solganeo.com>
2024-06-28 17:11:46 +0200

    Deactivate init stuff 4

--- routes/init.js
+++ routes/init.js
@@ -11,22 +11,13 @@ const client = new Client({
 });*/
 
 router.get('/', (req, res) => {
-    fs.readFile(path.join(__dirname, '../views', 'init.html'), 'utf8', (err, html) => {
+    res.send("<h1>LUL</h1>");
+    /*fs.readFile(path.join(__dirname, '../views', 'init.html'), 'utf8', (err, html) => {
         if (err) {
             console.error('Failed to read the HTML file', err);
             res.status(500).send('An error occurred while reading the HTML file.');
         } else {
-
-            /*client.connect();
-            client.query('SELECT table_schema,table_name FROM information_schema.tables WHERE  table_schema = "AppSchema" AND  table_name  = "AppTable"', (err, res) => {
-                if (err) throw err;
-                for (let row of res.rows) {
-                    console.log(JSON.stringify(row));
-                }
-                client.end();
-            });*/
-
             res.send(html);
         }
-    });
+    });*/
 });
