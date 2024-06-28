ea383e928a4208b6cf2d65b46fe2c0aab1b12204
Thomas Crenn <t.crenn@solganeo.com>
2024-06-28 17:50:38 +0200

    Experiment with config file

--- classes/database.js
+++ classes/database.js
@@ -1,7 +1,10 @@
 const { Client } = require('pg');
+const config = require('../config/default.json');
 
 class DataBase {
     constructor() {
+        this.schema = config.get('db.schema');
+        this.table = config.get('db.table');
         this.client = new Client({
             connectionString: process.env.DATABASE_URL,
             ssl: {
@@ -16,8 +19,8 @@ class DataBase {
         this.client.query(`SELECT EXISTS (
             SELECT 1
             FROM   information_schema.tables 
-            WHERE  table_schema = 'SchemaName'
-            AND    table_name = 'TableName'
+            WHERE  table_schema = '${this.schema}'
+            AND    table_name = '${this.table}'
         );`, (err, res) => {
             if (err) throw err;
             if(res.rows.length > 0) {
